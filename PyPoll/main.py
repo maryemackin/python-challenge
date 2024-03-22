26a0785d60f36cf62992fe754d9e1e7d70ddaaab
maryemackin <140846872+maryemackin@users.noreply.github.com>
2024-03-21 17:56:16 -0700

    save checkpoint UNTESTED code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,8 +3,8 @@ import os
 import csv
 
 # Set path/file to import/export
-election_data_csv = os.path.join("Resources", "election_data.csv")
-election_data_analysis_txt = os.path.join("analysis", "election_data_analysis.txt")
+election_data_csv = os.path.join("PyPoll", "Resources", "election_data.csv")
+election_data_analysis_txt = os.path.join("PyPoll", "analysis", "election_data_analysis.txt")
 
 # create variables for analysis and set start values & empty lists/dictionaries to hold analysis results
 totalVotes = 0
@@ -12,6 +12,9 @@ candidates = []
 candidateVotes = {}
 winner = ""
 winningVoteTotals = 0
+candidateName = ""
+votes = 0
+
 
 # open and read csv file
 with open(election_data_csv) as electionData:
@@ -40,18 +43,51 @@ with open(election_data_csv) as electionData:
             # create starting value for all candidates' vote counts
             candidateVotes[candidateName] = 0
         
-        candidateVotes[candidateNames] += 1
+        # update vote count with new votes
+        candidateVotes[candidateName] += 1
 
+        # results/analysis
 
-# results/analysis
-electionFinalResults = (f"Election Results\n"
+        # variables for summary
+        votePercent = float(votes) / float(totalVotes) * 100
+        votes = candidateVotes(candidates)
+ 
+        electionFinal = f"{candidates}: {votePercent:.3f}% ({votes})\n"
+
+        
+        electionAnalysis = (f"Election Results\n"
                         f"-------------------------\n"
                         f"Total Votes: {totalVotes}\n"
                         f"-------------------------\n"
+                        f"{electionFinal}"
+                        f"-------------------------\n"
+                        f"Winner: {winner}\n"
+                        f"-------------------------\n")
+
+
+        # print result and clean up spacing for summary
+        print(electionFinal, end="")
+
+        txt_file.write("election_data_analysis.txt")
+
+        # create for loop with an if statement to calculate winner
+        for candidate in candidateVotes:            
+
+            if(votes > winningVoteTotals):
+                winningVoteTotals = votes
+                winnerName = candidate
+
+            # print result and clean up spacing for summary
+            print(electionFinal, end="")
+
+
+
+
+
+
+
 
 
 # export analysis as a text file
 with open(election_data_analysis_txt, "w") as txt_file:
     txt_file.write("election_data_analysis.txt")
-
-    
